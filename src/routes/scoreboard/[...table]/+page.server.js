5b3a2f0b7d05872b31ee6d7462d7989c6e10db63
Finn <30553137+finncyr@users.noreply.github.com>
2023-01-23 12:40:07 +0000

    header design

--- src/routes/scoreboard/[...table]/+page.server.js
+++ src/routes/scoreboard/[...table]/+page.server.js
@@ -19,10 +19,12 @@ async function fetchData(tableURL) {
     const logos = tableBody.find("tr > td:nth-child(2) > a > span.table-cell-item:nth-child(1) > img").toArray().map(el => $(el).attr("data-src"));
     const wins = tableBody.find("tr > td:nth-child(3) > a").toArray().map(el => parseInt($(el).text(), 10));
     const losses = tableBody.find("tr > td:nth-child(4) > a").toArray().map(el => parseInt($(el).text(), 10));
+    
+    const league = $('.page-title').text();
 
     if (!teams.length) return {error: "Not a valid URL"}
   
-    return {teams: teams.map((team, i) => ({name: team, wins: wins[i], losses: losses[i], logo: logos[i]}))};
+    return {teams: teams.map((team, i) => ({name: team, wins: wins[i], losses: losses[i], logo: logos[i]})), leaguename: league};
   } catch(err) {
     return {error: "Failed to parse URL"}
   }
--- src/routes/scoreboard/[...table]/+page.svelte
+++ src/routes/scoreboard/[...table]/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
+    let headeron = true; /*To be implmented in Query*/
+    let ligalogo = "https://yt3.googleusercontent.com/5ThjpQMcF5t_wwKLfarGVdo8n-gfTVCZ89IKJMMgI00gVJ7LIc0Jt6W1o28uERDy1xbsOQ078A=s900-c-k-c0x00ffffff-no-rj";
 </script>
 
 <svelte:head>
@@ -11,6 +13,17 @@
 
 {#if data.teams}
     <ul class="teams">
+        {#if headeron}
+        <li class="header">
+            <img class="logo" src={ligalogo} alt="Liga Logo" width="80" height="80">
+            <div class="header-data">
+                <span class="header-name">{data.leaguename}</span>
+                <span class="header-wins">W</span>
+                <span class="header-losses">L</span>
+            </div>
+        </li>
+        <li class="spacer"></li>
+        {/if}
     {#each data.teams as team}
         <li class="team">
             <img class="logo" src={team.logo} alt="Team Logo" width="80" height="80">
@@ -32,7 +45,7 @@
         width: 1920px;
         margin: 0;
         padding: 0;
-        /* background-color: rgba(0, 0, 0, 0.1); Delete this for production */
+        background-color: rgba(0, 0, 0, 0.1);
         font-family: 'Inter', sans-serif;
         display: flex;
         justify-content: center;
@@ -41,6 +54,43 @@
     .teams {
         list-style-type: none;
     }
+    .header {
+        display: flex;
+        margin: 20px;
+        color: #111;
+    }
+    .header-data {
+        width: 710px;
+        height: 80px;
+
+        margin: 0 30px;
+        display: flex;
+        align-items: center;
+        gap: 40px;
+
+        font-weight: bolder;
+        font-size: 30px;
+        line-height: 50px;
+        text-transform: uppercase;
+    }
+    .header-data > .header-name {
+        width: 550px;
+    }
+    .header-data > .header-wins {
+        width: 40px;
+        text-align: center;
+    }
+    .header-data > .header-losses {
+        width: 40px;
+        text-align: center;
+    } 
+    .spacer {
+        height: 3px;
+        margin: 0px 20px 0px 20px;
+        background-color: #111;
+        display: flex;
+    }
+
     .team {
         display: flex;
         margin: 20px;
